a4c026eb8a8014475adf66b4fc902ffc5b1b230b
Omar Safari <safari.omar03@gmail.com>
2026-06-02 05:54:09 +0200

    Ajout Dockerfile et Docker Compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'omar',
-  host: 'localhost',
-  database: 'bibliotheque',
-  password: 'omar123',
-  port: 5432,
+  user: process.env.DB_USER || 'omar',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'bibliotheque',
+  password: process.env.DB_PASSWORD || 'omar123',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
